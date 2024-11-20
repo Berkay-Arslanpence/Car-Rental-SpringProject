df56115e3c5fa8800a1f36fac7aa8e662469340c
OmerEmreBozkurt <omeremre34@hotmail.com>
2024-11-20 22:27:27 +0300

    DBCONNECTİON

--- carRental-app/src/main/java/com/rentCar/carRental_app/CarRentalAppApplication.java
+++ carRental-app/src/main/java/com/rentCar/carRental_app/CarRentalAppApplication.java
@@ -3,10 +3,13 @@ package com.rentCar.carRental_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+
 @SpringBootApplication
 public class CarRentalAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CarRentalAppApplication.class, args);
 	}
 
