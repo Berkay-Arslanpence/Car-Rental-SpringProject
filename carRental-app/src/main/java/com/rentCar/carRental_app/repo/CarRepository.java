803dcac071444cf6a27fe75176322a1daf69de11
OmerEmreBozkurt <omeremre34@hotmail.com>
2024-11-29 15:40:46 +0300

    ReturnCar method (resRepo and ResServ) search AvailableCars method

--- carRental-app/src/main/java/com/rentCar/carRental_app/repo/CarRepository.java
+++ carRental-app/src/main/java/com/rentCar/carRental_app/repo/CarRepository.java
@@ -1,7 +1,10 @@
 package com.rentCar.carRental_app.repo;
 
 import com.rentCar.carRental_app.model.Car;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,6 +17,15 @@ public interface CarRepository extends JpaRepository<Car, String> {
 
     Car findByBarcode(String barcode);
 
+    Car findById(long id);
 
     void deleteByBarcodeAndStatus(String barcode, Car.CarStatus status);
+
+    @Query("select c from Car c where c.status='AVAILABLE' and c.carType=?1 and c.transmissionType=?2 ")
+    List<Car> searchAvailableCars(String carType, String transmissionType);
+
+    @Transactional
+    @Modifying
+    @Query("update Car c set c.status = com.rentCar.carRental_app.model.Car.CarStatus.AVAILABLE where c.barcode = ?1")
+    int updateCarStatusToAvailable(String barcode);
 }
