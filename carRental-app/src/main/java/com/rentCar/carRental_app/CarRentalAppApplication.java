2d7eca1430c1e152d596ef6168e62ab63d5882b3
Ömer Emre Bozkurt <omeremre34@hotmail.com>
2024-12-11 18:01:29 +0300

    swagger bıdısı

--- carRental-app/src/main/java/com/rentCar/carRental_app/CarRentalAppApplication.java
+++ carRental-app/src/main/java/com/rentCar/carRental_app/CarRentalAppApplication.java
@@ -1,11 +1,13 @@
 package com.rentCar.carRental_app;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.Connection;
+
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class CarRentalAppApplication {
 
 	public static void main(String[] args) {
